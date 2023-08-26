d50415fd5e0cec8b81261ef4395359d845a6ed88
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-26 18:38:26 +0600

    Updated

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+// route not found
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Route Not Found'
+    })
+})
+
+// handle Server Error
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: 'Server Error'
+    })
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is RUNNING on port ${process.env.PORT}`)
